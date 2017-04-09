d7800f74d4a30e06d5680f418cc6bef209eb2864
viksmicro <coolvicky.soni@gmail.com>
2017-04-09 05:18:38 -0700

    added comments

--- Trees-and-Graphs/Validate-BST/Validate-BST.cpp
+++ Trees-and-Graphs/Validate-BST/Validate-BST.cpp
@@ -9,8 +9,13 @@ bool IsBst_recur(binary_tree_node<int>* node, int min, int max)
 {
 	if (node == nullptr)
 		return true;
+	// if node violates the BST property, then return false
 	if ((node->value < min) || (node->value > max))
 		return false;
+	// check for each node.
+	// when traversing left side of the current node, we need to only make sure if the values are between
+	// min and node value-1. Similarly for right side of the node, we need to make sure that values are between node_value+1
+	// and max
 	if ((!IsBst_recur(node->left, min, node->value - 1))
 		|| (!IsBst_recur(node->right, node->value + 1, max)))
 		return false;
@@ -19,13 +24,16 @@ bool IsBst_recur(binary_tree_node<int>* node, int min, int max)
 
 bool IsBst(binary_tree_node<int>* root)
 {
+	// base case
 	if (root == nullptr)
 		return true;
+	// we will start with min and max values
 	return IsBst_recur(root, INT_MIN, INT_MAX);
 }
 
 int main()
 {
+	//  5 test cases
 	std::cout << "\n Is tree a BST 1: " << IsBst(nullptr);
 	binary_tree_node<int>* root = new binary_tree_node<int>(15);
 	std::cout << "\n Is tree a BST 2: " << IsBst(root);
